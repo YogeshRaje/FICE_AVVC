4c3fb2f7bd9897b7f796df94768f94551b690a58
Yogesh Raje <yhraje@gmail.com>
2023-02-03 13:46:41 +0530

    Add files via upload

---
+++ demo_python1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import time
+
+
+t0=time.time()  
+print("Welcome to Learn Intel Python")
+p_time=time.time()-t0
+print("Execution_time in  micro seconds  :", p_time*1000000)
+
+
+# In[ ]:
+
+
+
+
