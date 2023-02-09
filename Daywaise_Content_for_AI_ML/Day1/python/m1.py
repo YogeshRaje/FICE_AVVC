8dad4a88ddaa6967c9d6bf344aa98310f6ab7b44
Yogesh Raje <yhraje@gmail.com>
2023-02-09 14:14:25 +0530

    Add files via upload

---
+++ Daywaise_Content_for_AI_ML/Day1/python/m1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+def fun1():
+    printf("This is function1")
+    
+def addition(a,b):
+    return a+b
+val=20
+
+
+# In[5]:
+
+
+addition(1,7)
+
+
+# In[ ]:
+
+
+
+
+
+# In[8]:
+
+
+val
+
+
+# In[ ]:
+
+
+
+
